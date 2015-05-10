bc9e67b529928c9380b337a24b2166fbbae30d5c
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-05-10 13:32:27 +0800

    fix fancybox wrapper
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- theme/js/blogit-fancybox.js
+++ theme/js/blogit-fancybox.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
     $(".fancybox").fancybox();
 
     // Parse html and add fancybox support
-    var block = document.getElementsByClassName('figure');
-    for(var i = 0, l = block.length; i < l; i++) {
-        block[i].innerHTML = block[i].innerHTML.replace(/(\<img src=\"(.+)\"\s([^/]+)\>)/, '<a class="fancybox" rel="group" href="$2">$1</a>');
-    }
+    $(document).ready(function() {
+        $("p img").each(function() {
+            $(this).wrapAll('<a class="fancybox" href="' + this.src + '"></a>');
+        });
+    });
+
 });
