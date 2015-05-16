4117fb67aa94dd3b80faaa88ae4b5a3f0d0e64f8
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-05-16 14:18:38 +0800

    make img center
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- theme/js/blogit-fancybox.js
+++ theme/js/blogit-fancybox.js
@@ -4,10 +4,10 @@
 $(document).ready(function() {
     $(".fancybox").fancybox();
 
-    // Parse html and add fancybox support
-    $(document).ready(function() {
-        $("p img").each(function() {
-            $(this).wrapAll('<a class="fancybox" href="' + this.src + '"></a>');
-        });
-    });
+    // // Parse html and add fancybox support
+    // $(document).ready(function() {
+    //     $("p img").each(function() {
+    //         $(this).wrapAll('<a class="fancybox" href="' + this.src + '"></a>');
+    //     });
+    // });
 });
