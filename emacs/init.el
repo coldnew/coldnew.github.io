a6e8caa67970a06560ff56ca83ba1070a630e10b
Yen-Chin Lee <coldnew.tw@gmail.com>
2017-04-14 21:35:20 +0800

    Add emacs setup
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ emacs/init.el
@@ -0,0 +1,47 @@
+;;; init.el
+
+;; Copyright (c) 2017 Yen-Chin, Lee.
+;;
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+(defvar config-path
+  (file-name-directory (or load-file-name (buffer-file-name))))
+
+;; load my-theme
+(load (expand-file-name "coldnew-theme.el" config-path))
+(load (expand-file-name "night-coldnew-theme.el" config-path))
+
+;; use night-coldnew theme
+(load-theme 'night-coldnew t)
+
+;;;; u-mode is some example I write for my blog post
+(require 'generic-x)
+
+(define-generic-mode 'u-mode
+  ;; comments
+  '(("//")  ("/*" . "*/"))
+  ;; keywords
+  '("Name" "Tel" "Password")
+  ;; other syntax
+  nil
+  ;; filetype
+  '("\\.u$")
+  ;; init func
+  nil
+  "a emacs major-mode for u-language")
