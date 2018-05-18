666cc518445304c0e30c75d828a9abea2c1dc79b
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-05-18 15:08:36 +0800

    .dir-locals.el: add org-download support
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,21 @@
-((nil . ((hexo-new-format . org))
-      ))
+((nil .
+      ((eval .
+	     (progn
+
+	       ;; make drag-and-drop image save in the same name folder as org file
+	       ;; ex: `aa-bb-cc.org' then save image test.png to `aa-bb-cc/test.png'
+	       (defun my-org-download-method (link)
+		 (let ((filename
+			(file-name-nondirectory
+			 (car (url-path-and-query
+			       (url-generic-parse-url link)))))
+		       (dirname (file-name-sans-extension (buffer-name)) ))
+		   (make-directory dirname)
+		   (expand-file-name filename dirname)))
+
+	       (setq org-download-method 'my-org-download-method)
+
+	       ;; setup for hexo.el
+	       (setq hexo-new-format 'org)
+
+	       )))))
