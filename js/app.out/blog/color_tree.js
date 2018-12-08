e00d47a9c6f4e93553c214f5e04f967ec8b163b0
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-12-08 18:04:40 +0800

    Site updated: 2018-12-08 18:04:02

--- js/app.out/blog/color_tree.js
+++ js/app.out/blog/color_tree.js
@@ -7,11 +7,11 @@ goog.require('clojure.string');
  *   I use this function due to current cljs doesn't support #"(?m)^xxx$" regexp, which work on clojure.
  */
 blog.color_tree.replace_line_starts = (function blog$color_tree$replace_line_starts(regex,newval,seq){
-return cljs.core.map.cljs$core$IFn$_invoke$arity$2((function (p1__12090_SHARP_){
-if(clojure.string.starts_with_QMARK_(p1__12090_SHARP_,clojure.string.join.cljs$core$IFn$_invoke$arity$1(cljs.core.rest(cljs.core.re_find(regex,p1__12090_SHARP_))))){
-return clojure.string.replace(p1__12090_SHARP_,regex,newval);
+return cljs.core.map.cljs$core$IFn$_invoke$arity$2((function (p1__12097_SHARP_){
+if(clojure.string.starts_with_QMARK_(p1__12097_SHARP_,clojure.string.join.cljs$core$IFn$_invoke$arity$1(cljs.core.rest(cljs.core.re_find(regex,p1__12097_SHARP_))))){
+return clojure.string.replace(p1__12097_SHARP_,regex,newval);
 } else {
-return p1__12090_SHARP_;
+return p1__12097_SHARP_;
 }
 }),seq);
 });
@@ -19,55 +19,6 @@ blog.color_tree.tree = (function blog$color_tree$tree(x1,x2,x3,color){
 return [cljs.core.str(x1),cljs.core.str("<font color=\""),cljs.core.str(color),cljs.core.str("\">"),cljs.core.str(x2),cljs.core.str("</font>"),cljs.core.str(x3)].join('');
 });
 blog.color_tree.treeB = (function blog$color_tree$treeB(var_args){
-var args12091 = [];
-var len__7511__auto___12094 = arguments.length;
-var i__7512__auto___12095 = (0);
-while(true){
-if((i__7512__auto___12095 < len__7511__auto___12094)){
-args12091.push((arguments[i__7512__auto___12095]));
-
-var G__12096 = (i__7512__auto___12095 + (1));
-i__7512__auto___12095 = G__12096;
-continue;
-} else {
-}
-break;
-}
-
-var G__12093 = args12091.length;
-switch (G__12093) {
-case 3:
-return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$3((arguments[(0)]),(arguments[(1)]),(arguments[(2)]));
-
-break;
-case 2:
-return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2((arguments[(0)]),(arguments[(1)]));
-
-break;
-case 1:
-return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$1((arguments[(0)]));
-
-break;
-default:
-throw (new Error([cljs.core.str("Invalid arity: "),cljs.core.str(args12091.length)].join('')));
-
-}
-});
-
-blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$3 = (function (x1,x2,x3){
-return blog.color_tree.tree(x1,x2,x3,"lightblue");
-});
-
-blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2 = (function (x1,x2){
-return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$3(x1,x2,"");
-});
-
-blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$1 = (function (x2){
-return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$3("",x2,"");
-});
-
-blog.color_tree.treeB.cljs$lang$maxFixedArity = 3;
-blog.color_tree.treeR = (function blog$color_tree$treeR(var_args){
 var args12098 = [];
 var len__7511__auto___12101 = arguments.length;
 var i__7512__auto___12102 = (0);
@@ -86,15 +37,15 @@ break;
 var G__12100 = args12098.length;
 switch (G__12100) {
 case 3:
-return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$3((arguments[(0)]),(arguments[(1)]),(arguments[(2)]));
+return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$3((arguments[(0)]),(arguments[(1)]),(arguments[(2)]));
 
 break;
 case 2:
-return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2((arguments[(0)]),(arguments[(1)]));
+return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2((arguments[(0)]),(arguments[(1)]));
 
 break;
 case 1:
-return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$1((arguments[(0)]));
+return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$1((arguments[(0)]));
 
 break;
 default:
@@ -103,20 +54,20 @@ throw (new Error([cljs.core.str("Invalid arity: "),cljs.core.str(args12098.lengt
 }
 });
 
-blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$3 = (function (x1,x2,x3){
-return blog.color_tree.tree(x1,x2,x3,"#ff3232");
+blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$3 = (function (x1,x2,x3){
+return blog.color_tree.tree(x1,x2,x3,"lightblue");
 });
 
-blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2 = (function (x1,x2){
-return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$3(x1,x2,"");
+blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2 = (function (x1,x2){
+return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$3(x1,x2,"");
 });
 
-blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$1 = (function (x2){
-return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$3("",x2,"");
+blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$1 = (function (x2){
+return blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$3("",x2,"");
 });
 
-blog.color_tree.treeR.cljs$lang$maxFixedArity = 3;
-blog.color_tree.treeG = (function blog$color_tree$treeG(var_args){
+blog.color_tree.treeB.cljs$lang$maxFixedArity = 3;
+blog.color_tree.treeR = (function blog$color_tree$treeR(var_args){
 var args12105 = [];
 var len__7511__auto___12108 = arguments.length;
 var i__7512__auto___12109 = (0);
@@ -135,6 +86,55 @@ break;
 var G__12107 = args12105.length;
 switch (G__12107) {
 case 3:
+return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$3((arguments[(0)]),(arguments[(1)]),(arguments[(2)]));
+
+break;
+case 2:
+return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2((arguments[(0)]),(arguments[(1)]));
+
+break;
+case 1:
+return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$1((arguments[(0)]));
+
+break;
+default:
+throw (new Error([cljs.core.str("Invalid arity: "),cljs.core.str(args12105.length)].join('')));
+
+}
+});
+
+blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$3 = (function (x1,x2,x3){
+return blog.color_tree.tree(x1,x2,x3,"#ff3232");
+});
+
+blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2 = (function (x1,x2){
+return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$3(x1,x2,"");
+});
+
+blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$1 = (function (x2){
+return blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$3("",x2,"");
+});
+
+blog.color_tree.treeR.cljs$lang$maxFixedArity = 3;
+blog.color_tree.treeG = (function blog$color_tree$treeG(var_args){
+var args12112 = [];
+var len__7511__auto___12115 = arguments.length;
+var i__7512__auto___12116 = (0);
+while(true){
+if((i__7512__auto___12116 < len__7511__auto___12115)){
+args12112.push((arguments[i__7512__auto___12116]));
+
+var G__12117 = (i__7512__auto___12116 + (1));
+i__7512__auto___12116 = G__12117;
+continue;
+} else {
+}
+break;
+}
+
+var G__12114 = args12112.length;
+switch (G__12114) {
+case 3:
 return blog.color_tree.treeG.cljs$core$IFn$_invoke$arity$3((arguments[(0)]),(arguments[(1)]),(arguments[(2)]));
 
 break;
@@ -147,7 +147,7 @@ return blog.color_tree.treeG.cljs$core$IFn$_invoke$arity$1((arguments[(0)]));
 
 break;
 default:
-throw (new Error([cljs.core.str("Invalid arity: "),cljs.core.str(args12105.length)].join('')));
+throw (new Error([cljs.core.str("Invalid arity: "),cljs.core.str(args12112.length)].join('')));
 
 }
 });
@@ -177,19 +177,19 @@ var length = block.length;
 var i = (0);
 while(true){
 if((i < length)){
-var target_12114 = (block[i]);
-target_12114.innerHTML = clojure.string.join.cljs$core$IFn$_invoke$arity$2("\n",cljs.core.map.cljs$core$IFn$_invoke$arity$2(((function (i,target_12114,block,length){
-return (function (p1__12113_SHARP_){
-return clojure.string.replace(p1__12113_SHARP_,/([\w\-\.]*)(&lt;g&gt;)/,"<font color=\"lightgreen\">$1</font>");
-});})(i,target_12114,block,length))
-,cljs.core.map.cljs$core$IFn$_invoke$arity$2(((function (i,target_12114,block,length){
-return (function (p1__12112_SHARP_){
-return clojure.string.replace(p1__12112_SHARP_,/(\w*)(&lt;b&gt;)/,"<font color=\"lightblue\">$1</font>");
-});})(i,target_12114,block,length))
-,blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├─└─\s]*\s*)(.*\s*)(-&gt;)(.*\s*)(&lt;lw&gt;)/,blog.color_tree.treeLW("$1","$2","$3","$4"),blog.color_tree.replace_line_starts(/([├─└─\s]*\s*)(.*\s*)(-&gt;)(.*\s*)(&lt;lw&gt;)/,blog.color_tree.treeLW("$1","$2","$3","$4"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├─└─\s]*\s*)(.*\s*)(-&gt;)(.*\s*)(&lt;lb&gt;)/,blog.color_tree.treeLB("$1","$2","$3","$4"),blog.color_tree.replace_line_starts(/([├─└─\s]*\s*)(.*\s*)(-&gt;)(.*\s*)(&lt;lb&gt;)/,blog.color_tree.treeLB("$1","$2","$3","$4"),blog.color_tree.replace_line_starts(/([├─└─]*\s*)([\w\-\.]*\s*)(&lt;g&gt;)/,blog.color_tree.treeG.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s│&nbsp;&nbsp;\s[├─└─\s]*\s*)([\w\-\.]*\s*)(&lt;g&gt;)/,blog.color_tree.treeG.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├─└─\s]*\s*)([\w\-\.]*\s*)(&lt;g&gt;)/,blog.color_tree.treeG.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s│&nbsp;&nbsp;\s[├─└─\s]*\s*)([\w\-\.]*\s*)(&lt;r&gt;)/,blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├─└─]*\s*)([\w\-\.]*\s*)(&lt;r&gt;)/,blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/([├─]*\s*)([\w\-\.]*\s*)(&lt;r&gt;)/,blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s│&nbsp;&nbsp;\s[├─└─\s]*\s*)([\w\-\.]*\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├─└─]*\s*)([\w\-\.]*\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/([├─└─]*\s*)([\w\-\.]*\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(\/.*\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$1("$1"),blog.color_tree.replace_line_starts(/(\.\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$1("$1"),clojure.string.split.cljs$core$IFn$_invoke$arity$2(target_12114.innerHTML,/\n/)))))))))))))))))));
+var target_12121 = (block[i]);
+target_12121.innerHTML = clojure.string.join.cljs$core$IFn$_invoke$arity$2("\n",cljs.core.map.cljs$core$IFn$_invoke$arity$2(((function (i,target_12121,block,length){
+return (function (p1__12120_SHARP_){
+return clojure.string.replace(p1__12120_SHARP_,/([\w\-\.]*)(&lt;g&gt;)/,"<font color=\"lightgreen\">$1</font>");
+});})(i,target_12121,block,length))
+,cljs.core.map.cljs$core$IFn$_invoke$arity$2(((function (i,target_12121,block,length){
+return (function (p1__12119_SHARP_){
+return clojure.string.replace(p1__12119_SHARP_,/(\w*)(&lt;b&gt;)/,"<font color=\"lightblue\">$1</font>");
+});})(i,target_12121,block,length))
+,blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├──├─└─\s]*\s*)(.*\s*)(-&gt;)(.*\s*)(&lt;lw&gt;)/,blog.color_tree.treeLW("$1","$2","$3","$4"),blog.color_tree.replace_line_starts(/([├──├─└─\s]*\s*)(.*\s*)(-&gt;)(.*\s*)(&lt;lw&gt;)/,blog.color_tree.treeLW("$1","$2","$3","$4"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├──├─└─\s]*\s*)(.*\s*)(-&gt;)(.*\s*)(&lt;lb&gt;)/,blog.color_tree.treeLB("$1","$2","$3","$4"),blog.color_tree.replace_line_starts(/([├──├─└─\s]*\s*)(.*\s*)(-&gt;)(.*\s*)(&lt;lb&gt;)/,blog.color_tree.treeLB("$1","$2","$3","$4"),blog.color_tree.replace_line_starts(/([├──├─└─]*\s*)([\w\-\.]*\s*)(&lt;g&gt;)/,blog.color_tree.treeG.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s│&nbsp;&nbsp;\s[├──├─└─\s]*\s*)([\w\-\.]*\s*)(&lt;g&gt;)/,blog.color_tree.treeG.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├──├─└─\s]*\s*)([\w\-\.]*\s*)(&lt;g&gt;)/,blog.color_tree.treeG.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s│&nbsp;&nbsp;\s[├──├─└─\s]*\s*)([\w\-\.]*\s*)(&lt;r&gt;)/,blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├──├─└─]*\s*)([\w\-\.]*\s*)(&lt;r&gt;)/,blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/([├──├─]*\s*)([\w\-\.]*\s*)(&lt;r&gt;)/,blog.color_tree.treeR.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s│&nbsp;&nbsp;\s[├─└─\s]*\s*)([\w\-\.]*\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(│&nbsp;&nbsp;\s*[├─└─]*\s*)([\w\-\.]*\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/([├──├─└─]*\s*)([\w\-\.]*\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$2("$1","$2"),blog.color_tree.replace_line_starts(/(\/.*\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$1("$1"),blog.color_tree.replace_line_starts(/(\.\s*)(&lt;b&gt;)/,blog.color_tree.treeB.cljs$core$IFn$_invoke$arity$1("$1"),clojure.string.split.cljs$core$IFn$_invoke$arity$2(target_12121.innerHTML,/\n/)))))))))))))))))));
 
-var G__12115 = (i + (1));
-i = G__12115;
+var G__12122 = (i + (1));
+i = G__12122;
 continue;
 } else {
 return null;
