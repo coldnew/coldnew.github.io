4fe93c06f99c00caa1ab128a15385aae51910b7f
Yen-Chin, Lee <coldnew.tw@gmail.com>
2026-02-24 19:35:11 +0800

    feat: add mdx-components for code block rendering

---
+++ src/mdx-components.tsx
@@ -0,0 +1,23 @@
+import {
+  CodeBlock,
+  CodeBlockTab,
+  CodeBlockTabs,
+  CodeBlockTabsList,
+  CodeBlockTabsTrigger,
+  Pre,
+} from 'fumadocs-ui/components/codeblock';
+import type { HTMLAttributes, ReactNode } from 'react';
+
+export const components = {
+  pre: (props: HTMLAttributes<HTMLPreElement> & { children?: ReactNode }) => (
+    <CodeBlock {...props}>
+      <Pre>{props.children}</Pre>
+    </CodeBlock>
+  ),
+  CodeBlockTab,
+  CodeBlockTabs,
+  CodeBlockTabsList,
+  CodeBlockTabsTrigger,
+};
+
+export default components;
